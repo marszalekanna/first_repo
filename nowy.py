b098ea58731c85e6922207563a8208b7dda064aa
Anna_Laura <marszalek.annalaura@gmail.com>
2023-02-04 21:02:56 +0100

    druga zmiana

---
+++ nowy.py
@@ -0,0 +1,13 @@
+# def zmien_na_liste(string):
+#     lista = list(string.split())
+#     return lista
+#
+# mojaLista = zmien_na_liste(input("wpisz kilka wyrazó≥w oddzielonych spacja: "))
+# print(mojaLista[-4])
+
+def zrobmyDrugaListe(string2):
+    lista2 = []
+    lista2[:0] = string2
+    return lista2
+
+print(zrobmyDrugaListe(input("Wpisz znow kilka wyrazów: ")))
