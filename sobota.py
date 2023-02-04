46373a916cd75fa574cf4997b78169007fef35e3
Anna_Laura <marszalek.annalaura@gmail.com>
2023-02-04 20:19:11 +0100

    04.02.23r

--- sobota.py
+++ sobota.py
@@ -1,1 +1,11 @@
 print("piekny dzien - numer 1")
+
+def wypisz_imie(my_name):
+    if my_name == "Ania":
+        return my_name
+    else:
+        return "Błąd"
+
+imie = wypisz_imie(input("Wpisz imię: "))
+
+print(imie)
