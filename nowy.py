30e3061f0affba3c99f7e0bfb41dd462a500a15f
Anna_Laura <marszalek.annalaura@gmail.com>
2023-02-05 20:45:24 +0100

    Trzecia zmiana

--- nowy.py
+++ nowy.py
@@ -5,17 +5,18 @@
 # mojaLista = zmien_na_liste(input("wpisz kilka wyrazó≥w oddzielonych spacja: "))
 # print(mojaLista[-4])
 
-# def zrobmyDrugaListe(string2):
+# def zrobmyDrugaListe(cokolwiek):
 #     lista2 = []
-#     lista2[:0] = string2
+#     lista2[:0] = cokolwiek
 #     return lista2
 
-string = input("wpisz tu cokolwiek: ")
-lista = list(string)
-print(lista)
-dodawane = list("nononono")
-lista.append(dodawane)
-print(lista)
+# string = input("wpisz tu cokolwiek: ")
+# lista = list(string)
+# print(lista)
+# dodawane = list("nononono")
+# lista.append(dodawane)
+#
+
 
 
 # print(zrobmyDrugaListe(input("Wpisz znow kilka wyrazów: ")))
